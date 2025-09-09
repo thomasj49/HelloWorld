a59763669477f7c4abfc85240c542db7cbade61f
ThomasJoe <thomasj49@nycstudents.net>
2025-09-09 13:19:33 -0400

    Added Scanner

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,17 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException{
         System.out.println("What's your name");
         Scanner s = new Scanner(System.in);
         String name= s.nextLine();
         System.out.println("Hello, " + name + "!");
+        File f = new File("Class.txt");
+        s = new Scanner(f);
+        String course = s.nextLine();
+        System.out.println("Welcome to " + course + "!");
         }
     }
